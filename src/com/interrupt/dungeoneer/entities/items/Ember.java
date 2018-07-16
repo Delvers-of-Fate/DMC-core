219b69cd87cd3b46bb39679599bdea4583d33a16
Drakensin <lightace1995@gmail.com>
2018-07-16 11:40:38 -0400

    Initial start for Core 1.04 Port

--- src/com/interrupt/dungeoneer/entities/items/Ember.java
+++ src/com/interrupt/dungeoneer/entities/items/Ember.java
@@ -1,28 +1,35 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.interrupt.dungeoneer.entities.items;
 
 import com.interrupt.dungeoneer.Audio;
 import com.interrupt.dungeoneer.annotations.EditorProperty;
 import com.interrupt.dungeoneer.entities.Item;
 import com.interrupt.dungeoneer.entities.Player;
+import com.interrupt.dungeoneer.entities.Entity.ArtType;
+import com.interrupt.dungeoneer.entities.Entity.CollidesWith;
+import com.interrupt.dungeoneer.entities.Item.ItemType;
 import com.interrupt.dungeoneer.game.Game;
 import com.interrupt.dungeoneer.game.Level;
 import com.interrupt.managers.StringManager;
-
 import java.text.MessageFormat;
 
-public class Gold extends Item {
+public class Ember extends Item {
     @EditorProperty
-    public int goldAmount;
+    public int emberAmount;
     public boolean autoPickup;
     public boolean playedDropSound;
 
-    public Gold() {
-        this.goldAmount = 1;
+    public Ember() {
+        this.emberAmount = 1;
         this.autoPickup = false;
         this.playedDropSound = false;
-        this.tex = 88;
+        this.tex = 100;
         this.artType = ArtType.item;
-        this.name = StringManager.get("items.Gold.defaultNameText");
+        this.name = StringManager.get("items.Ember.defaultNameText");
         this.collidesWith = CollidesWith.staticOnly;
         this.dropSound = "drops/drop_gold.mp3";
         this.collision.x = 0.1F;
@@ -30,38 +37,37 @@ public class Gold extends Item {
     }
 
     public Gold(float x, float y) {
-        super(x, y, 0, ItemType.gold, StringManager.get("items.Gold.defaultNameText"));
-        this.goldAmount = 1;
+        super(x, y, 0, ItemType.ember, StringManager.get("items.Ember.defaultNameText"));
+        this.emberAmount = 1;
         this.autoPickup = false;
         this.playedDropSound = false;
     }
 
-    public Gold(int amount) {
+    public Ember(int amount) {
         this();
-
-        this.goldAmount = amount;
-        this.name = StringManager.get("items.Gold.defaultNameText") + " x" + Integer.toString(amount);
-        if(this.goldAmount <= 0) {
-            this.goldAmount = 1;
+        this.emberAmount = amount;
+        this.name = StringManager.get("items.Ember.defaultNameText");
+        if (this.emberAmount <= 0) {
+            this.emberAmount = 1;
         }
 
-        if(this.goldAmount > 5) {
-            this.tex = 89;
+        if (this.emberAmount > 5) {
+            this.tex = 101;
         }
 
         this.pickupSound = "pu_gold.mp3";
     }
 
     public String GetItemText() {
-        return MessageFormat.format(StringManager.get("items.Gold.goldItemText"), this.goldAmount);
+        return MessageFormat.format(StringManager.get("items.Ember.goldItemText"), this.emberAmount);
     }
 
     public void tick(Level level, float delta) {
         super.tick(level, delta);
-        if(this.isActive && this.autoPickup) {
+        if (this.isActive && this.autoPickup) {
             Player p = Game.instance.player;
-            if(Math.abs(p.x + 0.5F - this.x) < 0.3F && Math.abs(p.y + 0.5F - this.y) < 0.3F) {
-                ++p.gold;
+            if (Math.abs(p.x + 0.5F - this.x) < 0.3F && Math.abs(p.y + 0.5F - this.y) < 0.3F) {
+                ++p.ember;
                 this.isActive = false;
             }
         }
@@ -69,10 +75,11 @@ public class Gold extends Item {
     }
 
     protected void pickup(Player player) {
-        if(this.isActive) {
-            player.gold += this.goldAmount;
+        if (this.isActive) {
+            player.ember += this.emberAmount;
             this.isActive = false;
             Audio.playSound(this.pickupSound, 0.3F, 1.0F);
+            this.makeItemPickupAnimation(player);
         }
 
     }
